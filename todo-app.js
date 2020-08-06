a958b26ad96bdcc3c601ecc80c359b01e20f8e8c
pavandot <pavanpk0850@gmail.com>
2020-08-06 08:08:03 +0530

    uuid4 fixed

--- todo-app.js
+++ todo-app.js
@@ -1,8 +1,10 @@
-let todos = getTodos() 
+'use strict'
+
+let todos = getSavedTodos()
 
 const filters = {
     searchText: '',
-    hideCompleted:false
+    hideCompleted: false
 }
 
 renderTodos(todos, filters)
@@ -11,20 +13,20 @@ document.querySelector('#search-text').addEventListener('input', (e) => {
     filters.searchText = e.target.value
     renderTodos(todos, filters)
 })
- 
-document.querySelector('#add-form').addEventListener('submit',(e) => {
+
+document.querySelector('#new-todo').addEventListener('submit', (e) => {
     e.preventDefault()
     todos.push({
-        id: uuidv4() ,
-        text:e.target.elements.addTodo.value,
+        id: uuidv4(),
+        text: e.target.elements.text.value,
         completed: false
     })
     saveTodos(todos)
-    renderTodos(todos,filters)
-    e.target.elements.addTodo.value=''
+    renderTodos(todos, filters)
+    e.target.elements.text.value = ''
 })
 
-document.querySelector('#check').addEventListener('change',(e) =>{
-    filters.hideCompleted=e.target.checked
-    renderTodos(todos,filters)
-} )
+document.querySelector('#hide-completed').addEventListener('change', (e) => {
+    filters.hideCompleted = e.target.checked
+    renderTodos(todos, filters)
+})
